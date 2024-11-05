c6095eb5d87f68f9eee47ca256460ed420f34c4e
MahammmadAtheekShaik <smahammadatheek@gmail.com>
2024-11-06 01:17:30 +0530

    main link is added

--- backend/server.js
+++ backend/server.js
@@ -821,7 +821,13 @@ connection.once('open', () => {
 // app.use(cors());
 
 // app.use(cors({ origin: 'https://jobshustle.onrender.com' }));
-app.use(cors({ origin: 'https://www.jobhustles.com/' }));
+
+// Define the allowed origins
+const allowedOrigins = ['https://jobhustles.com', 'https://www.jobhustles.com'];
+app.use(cors({
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 app.use(express.json());
 app.use(bodyParser.json());
